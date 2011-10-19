3f9b558c2106cc0d18aaadc299a9d421e1d426d6
Nicholas Tietz <ntietz@gmail.com>
2011-10-19 03:40:30 -0400

    Finished most of problem 3 and added a sample input for it.

--- prolog-assignment/3.pl
+++ prolog-assignment/3.pl
@@ -1,47 +1,59 @@
 
-student(_ - _ - _ - _ - _ - _ - _).
-studentName(Record, Name) :- Record = Name - _ - _ - _ - _ - _ - _.
-studentAssignments(Record, [A1,A2,A3]) :- Record = _ - A1 - A2 - A3 - _ - _ - _.
-studentExams(Record, [E1,E2]) :- Record = _ - _ - _ - _ - E1 - E2 - _.
-studentFinal(Record,F) :- Record = _ - _ - _ - _ - _ - _ - F.
+student(_ - _ - _ - _ - _ - _ - _ - _ - _ - _).
+studentName(Record, Name) :- Record = Name - _ - _ - _ - _ - _ - _ - _ - _ - _.
+studentAttendance(Record, Att) :- Record = _ - Att - _ - _ - _ - _ - _ - _ - _ - _.
+studentAssignments(Record, [A1,A2,A3,A4]) :- Record = _ - _ - A1 - A2 - A3 - A4 - _ - _ - _ - _.
+studentProjects(Record, [P1, P2]) :- Record = _ - _ - _ - _- _ - _ - P1 - P2 - _ - _.
+studentExams(Record, E1) :- Record = _ - _ - _ - _ - _ - _ - _ - _ - E1 - _.
+studentFinal(Record,F) :- Record = _ - _ - _ - _ - _ - _ - _ - _ - _ - F.
 
 readName(Stream,Record) :-
     read(Stream,Name),
     studentName(Record,Name).
 
+readAttendance(Stream,Record) :-
+    read(Stream,Att),
+    studentAttendance(Record,Att).
+
 readAssignments(Stream,Record) :-
     read(Stream,A1),
     read(Stream,A2),
     read(Stream,A3),
-    studentAssignments(Record,[A1,A2,A3]).
+    read(Stream,A4),
+    studentAssignments(Record,[A1,A2,A3,A4]).
+
+readProjects(Stream,Record) :-
+    read(Stream,P1),
+    read(Stream,P2),
+    studentProjects(Record,[P1,P2]).
 
 readExams(Stream,Record) :-
     read(Stream,E1),
-    read(Stream,E2),
-    studentExams(Record,[E1,E2]).
+    studentExams(Record,E1).
 
 readFinal(Stream,Record) :-
     read(Stream,Final),
-    studentFinal(Record,Final).
+    studentFinal(Record,Final),
+    print(Record), nl.
 
 processStudent(InFile) :-
     openFileToRead(InFile, InStream),
     processStudent(InStream, Student),
-    print('Finished processing.'),
     finalGrade(Student, FinalGrade),
     print(FinalGrade), nl,
-    print(Student).
-
-printer(X) :- print('Hi.').
+    print(Student), nl, print('--').
 
 processStudent(InStream, Student) :-
     student(Record),
     readName(InStream, Record),
+    readAttendance(InStream, Record),
     readAssignments(InStream, Record),
+    readProjects(InStream, Record),
     readExams(InStream, Record),
-    readFinal(InStream, Record).
+    readFinal(InStream, Record),
+    Student = Record.
 
 openFileToRead(InFile, InStream) :- open(InFile, read, InStream).
 
-finalGrade(Name - A1 - A2 - A3 - E1 - E2 - F, FinalGrade) :- FinalGrade = F.
+finalGrade(Name - Att - A1 - A2 - A3 - A4 - P1 - P2 - E1 - F, FinalGrade) :- FinalGrade is 0.1 * Att + 0.2 * (A1 + A2 + A3 + A4) / 4 + 0.1 * P1 + 0.25 * P2 + 0.15 * E1 + 0.2 * F.
 
