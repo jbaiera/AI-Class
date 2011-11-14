b2fed4b5adf028646e21c610bcb285af9bce7506
Nicholas Tietz <ntietz@gmail.com>
2011-11-13 22:39:57 -0500

    Added comments.

--- final-project/Reversi/Game.hs
+++ final-project/Reversi/Game.hs
@@ -5,8 +5,24 @@ boardSize :: Int
 boardSize = 8
 
 type Player = Int
+-- choices: 0 for unmarked
+--          1 for player 1
+--          2 for player 2
+
+type Direction = (Int, Int)
+type Position = (Int, Int)
 
 data Board = Board [[Player]] deriving (Show, Read, Eq)
 
---play :: Board -> (Int, Int) -> Board
+--play :: Board -> Player -> Position -> Board
+--play board player position = board'
+--    where directions = [ (a,b) | a <- [(-1),0,1], b <- [(-1),0,1],
+--                         flippable board player position (a,b) ]
+--          board' = something
+
+-- flippable determines whether or not we should flip tiles in that direction
+--flippable :: Board -> Player -> Position -> Direction -> Bool
+
+-- flip will flip all the tiles in one direction
+--flip :: Board -> Player -> Position -> Direction -> Board
 
