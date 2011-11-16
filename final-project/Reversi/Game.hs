b2c5e3b799c657777779672d8888e7b4fa8622a6
Nicholas Tietz <ntietz@gmail.com>
2011-11-16 10:21:49 -0500

    Fixed flip validation. Began fixing move-flipping code.

--- final-project/Reversi/Game.hs
+++ final-project/Reversi/Game.hs
@@ -15,38 +15,56 @@ type Position = (Int, Int)
 data Board = Board [[Player]] deriving (Show, Read, Eq)
 
 -- the default starting position
+initialGrid :: [[Int]]
+initialGrid = [[0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0],
+               [0,0,0,1,2,0,0,0],
+               [0,0,0,2,1,0,0,0],
+               [0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0]]
+
 initialBoard :: Board
-initialBoard = Board [[0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0],
-                      [0,0,0,1,2,0,0,0],
-                      [0,0,0,2,1,0,0,0],
-                      [0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0],
-                      [0,0,0,0,0,0,0,0]]
+initialBoard = Board initialGrid
 
 --play :: Board -> Player -> Position -> Board
 play board player position = board'
-    where directions = [ (a,b) | a <- [(-1),0,1], b <- [(-1),0,1],
+    where directions = [ (a,b) | a <- [(-1),0,1], b <- [(-1),0,1], (a,b) /= (0,0),
                                  valid board player position (a,b) ]
           board' = foldr (\d b -> capture b player position d) board directions
 
 -- valid determines whether or not we should flip tiles in that direction
 valid :: Board -> Player -> Position -> Direction -> Bool
 valid board@(Board grid) player position@(px, py) direction@(dx, dy)
-    | outOfBounds (px + dx, py + dy)               = False
-    | current == player || current == 0            = True
-    | otherwise                                    = valid board player (px+dx,py+dy) direction
-    where current = grid !! (px+dx) !! (py+dy)
+    | outOfBounds (px+dx, py+dy)                = False
+    | current == opponent && next == player     = True
+    | otherwise                                 = valid board player (px+dx, py+dy) direction
+    where current = grid !! px !! py
+          next = grid !! (px+dx) !! (py+dy)
+          opponent = case player of
+                1 -> 2
+                2 -> 1
+
+{-
+valid board@(Board grid) player position@(px, py) direction@(dx, dy)
+    | outOfBounds (px + dx, py + dy)                = False
+    | current == opponent && next == player         = True
+    | current == 0                                  = False
+    | otherwise                                     = valid board player (px+dx,py+dy) direction
+    where next    = grid !! (px+dx) !! (py+dy)
+          current = grid !! px !! py
+          opponent = if player == 1 then 2 else 1
+-}
 
 -- capture will flip all the tiles in one direction
 capture :: Board -> Player -> Position -> Direction -> Board
 capture board@(Board grid) player position@(px, py) direction@(dx, dy)
-    | outOfBounds (px + dx, py + dy) = board
-    | current == player || current == 0 = board
-    | otherwise = capture (Board grid') player (px + dx, py + dy) direction
+    | outOfBounds (px+dx, py+dy)    = board
+    | next == player                = board
+    | otherwise                     = capture (Board grid') player (px+dx, py+dy) direction
     where grid' = set grid position player
-          current = grid !! (px+dx) !! (py+dy)
+          next = grid !! (px+dx) !! (py+dy)
 
 -- outOfBounds checks if a position is a valid board position
 outOfBounds :: Position -> Bool
--- final-project/main.hs
+++ final-project/main.hs
@@ -2,5 +2,7 @@ import Reversi.Game
 
 main = do
     print initialBoard
-    print $ play initialBoard 1 (2,3)
+    print $ play initialBoard 1 (3,5)
+    print $ set initialGrid (3,2) 1
+    --print $ map (\d -> valid initialBoard 1 (2,3) d) [(x,y) | x <- [(-1)..1], y <- [(-1)..1] ]
 
