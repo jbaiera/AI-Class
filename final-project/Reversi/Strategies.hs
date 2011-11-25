4eccf4f08eae90f352ca6e7dcb63d41d6dd5ce20
Jeremy Dobbins <jdobbins@cs.kent.edu>
2011-11-25 03:49:41 -0500

    __Strategies__
    Added mobility function.
    
    Compiles and runs.

--- final-project/Reversi/Strategies.hs
+++ final-project/Reversi/Strategies.hs
@@ -14,18 +14,17 @@ greedy board@(Board grid) player = best
 --
 -- evaporation
 --
---   Arguments:
---     Strategy
+--   To be used with mobility strategy. This strategy tries to keep the
+--   number of pieces down during the first stage of the game, so as not to
+--   lose a massive block of pieces later in the game.
 --
---   Comments:
---     To be used with mobility strategy. This strategy tries to keep the
---     number of pieces down during the first stage of the game, so as not to
---     lose a massive block of pieces later in the game.
+--   This function simply finds the minimum path until move 17, when it will
+--   switch to the maximum value path. In essence, this is greedy.
 --
---     This function simply finds the minimum path until move 17, when it will
---     switch to the maximum value path. In essence, this is greedy.
+--   Arguments
+--     (Strategy)
 --
---   TODO:
+--   TODO
 --     Make the look-ahead go deeper.
 --     Put in a guard to avoid giving a complete win to the opponent.
 --
@@ -42,3 +41,32 @@ evaporation board@(Board grid) player
           worst = foldr (\x y -> if (better x y) then y else x)
                         (head moves) (tail moves)
           opening = if (move board < 19) then True else False
+
+--
+-- mobility
+--
+--   This function tries to increase the number of playable moves while
+--   decreasing them for the opponent.
+--
+--   Arguments
+--     (Strategy)
+--       Board:
+--       Player:
+--     
+--   Return
+--     (Strategy)
+--       Position: 
+--
+--   TODO
+--     Trace through to see if a player can guess the move of the opponent.  
+--
+
+mobility :: Strategy
+mobility board@(Board grid) player = best
+    where moves = possibleMoves board player
+          best = foldr (\x y -> if (better x y) then x else y)
+                       (head moves) (tail moves)
+          better x y = (length $ possibleMoves (play board player x) player)
+                     > (length $ possibleMoves (play board player y) player)
+
+
