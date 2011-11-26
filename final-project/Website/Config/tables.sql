892d396dc534b0dcc35d3f7889e64acd68518414
Nicholas Tietz <ntietz@gmail.com>
2011-11-26 12:07:13 -0500

    Added tables.

--- final-project/Website/Config/tables.sql
+++ final-project/Website/Config/tables.sql
@@ -6,3 +6,22 @@ create table login
     password varchar(60)
 );
 
+create table games
+(
+    game_id int not null primary key auto_increment,
+    game_type varchar(10),
+    player_1 int not null,
+    player_2 int not null,
+    start_time datetime,
+    end_time datetime
+);
+
+create table moves
+(
+    move_id int not null primary key auto_increment,
+    game_id int not null,
+    player int not null,
+    move_number int not null,
+    move varchar(10)
+);
+
