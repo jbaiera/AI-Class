dbfb7af8f2ead0bccdd655cb408db00c5964ef62
James Baiera <jbaiera@kent.edu>
2011-11-27 16:27:55 -0500

    Added some basic style, not a lot. Added piece images (How do I GIMP).

---
+++ final-project/Website/Public/resumegame.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+    <title>Let's Play - Reversi</title>
+    <meta charset="UTF-8" />
+    <meta name="keywords"   content="Student AI Project, artificial intellegence, games, reversi" />
+    <meta name"description" content="Play Reversi against the Machine!" />
+
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+    <div class="maincontainer">
+        <p> Board Goes Here </p>
+    </div>
+</body>
+</html>
