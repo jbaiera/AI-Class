6a28d03f39f5797fd219eef3b85a43cd5ed4bd51
Nicholas Tietz <ntietz@gmail.com>
2011-11-27 22:32:47 -0500

    Added program to read in then return a new board position.

--- final-project/Reversi/Interface.hs
+++ final-project/Reversi/Interface.hs
@@ -3,6 +3,8 @@ module Reversi.Interface where
 import Reversi.Game
 import Reversi.Strategies
 
+import System.Environment
+
 -- plays a game between two functions, printing the state of the board each time
 simulate :: Board -> Player -> Strategy -> Strategy -> IO ()
 simulate board player strategy1 strategy2
@@ -13,9 +15,6 @@ simulate board player strategy1 strategy2
     | otherwise    = do prettyPrint board
                         simulate (play board 2 $ strategy2 board player) 1 strategy1 strategy2
 
--- plays a game between a human and an AI
--- (TODO)
-
 -- plays a game between two functions, only printing the final status
 shortSimulate :: Board -> Player -> Strategy -> Strategy -> IO ()
 shortSimulate board player strategy1 strategy2
@@ -23,5 +22,14 @@ shortSimulate board player strategy1 strategy2
     | player == 1  = shortSimulate (play board 1 $ strategy1 board player) 2 strategy1 strategy2
     | otherwise    = shortSimulate (play board 2 $ strategy2 board player) 1 strategy1 strategy2
 
+-- reads a board, a move, and a player from the command line, then prints the moved board
+readAndPlay :: IO ()
+readAndPlay = do
+    args <- getArgs
+    let grid = read (args !! 0) :: [[Int]]
+    let position = read (args !! 1) :: Position
+    let player = read (args !! 2) :: Player
+    let (Board grid') = play (Board grid) player position
+    putStrLn $ show grid'
 
 
