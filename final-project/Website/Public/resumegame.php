874b15dce39cfa4333d387099ecf0a8be301c6df
Nicholas Tietz <ntietz@gmail.com>
2011-11-27 22:02:01 -0500

    Added path to ai programs, tested system commands.

--- final-project/Website/Public/resumegame.php
+++ final-project/Website/Public/resumegame.php
@@ -7,6 +7,10 @@ if (! isset($_SESSION['user_id']))
     header('Location: index.php');
 }
 
+$ai_location = '../..';
+
+// echo (system("pwd"));
+
 ?>
 
 <html>
@@ -25,3 +29,4 @@ if (! isset($_SESSION['user_id']))
     </div>
 </body>
 </html>
+
