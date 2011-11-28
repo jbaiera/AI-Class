cd3d745894a3b59cc742977fa6ee477c86b6c6ad
Nicholas Tietz <ntietz@gmail.com>
2011-11-27 23:06:07 -0500

    Added logic for creating new game, removed unneeded file.

--- final-project/Website/Public/newgame.php
+++ final-project/Website/Public/newgame.php
@@ -4,6 +4,41 @@ include('connect.php');
 
 // ADD LOGIC FOR MAKING NEW GAME HERE
 
+/* games table:
+    game_id     : auto_increment
+    game_type   : "reversi"
+    player_1    : either player or opponent (which is -1 for greedy, player for practice)
+    player_2    : either player or opponent (which is -1 for greedy, player for practice)
+    start_time  : (null)
+    end_time    : (null)
+    game_state  : begins as initialGrid
+
+*/
+
+if (isset($_POST['opponent'])) {
+    $player = $_SESSION['user_id'];
+
+    $oppString = $_POST['opponent'];
+    if ($oppString == "greedy") {
+        $opponent = -1;
+    } else if ($oppString == "practice") {
+        $opponent = $player;
+    }
+
+    if ($_POST['player'] == '1') {
+        $player_1 = $player;
+        $player_2 = $opponent;
+    } else {
+        $player_1 = $opponent;
+        $player_2 = $player;
+    }
+
+    // ADD LOGIC TO INSERT GAME IN TABLE
+
+    header('Location: resumegame.php');
+}
+
+var_dump($_POST);
 
 ?>
 
@@ -17,10 +52,15 @@ include('connect.php');
     <table>
         <tr>
             <td>Opponent
-            <td><select>
+            <td><select name="opponent">
                     <option value="greedy">Greedy</option>
                     <option value="practice">Practice</option>
                 </select>
+        <tr>
+            <td>Player
+            <td><select name="player">
+                    <option value="1">Black</option>
+                    <option value="2">White</option>
         <tr>
             <td>
             <td><input type="submit" />
