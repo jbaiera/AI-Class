d99ecf3b31360a98fed37081b168b45cd2d809c7
Nicholas Tietz <ntietz@gmail.com>
2011-11-27 22:15:13 -0500

    Fixed indentation, removed a var_dump.

--- final-project/Website/Public/resumegame.php
+++ final-project/Website/Public/resumegame.php
@@ -23,10 +23,10 @@ $board[5][6] = 3;
 
 <html>
 <head>
-    <title>Let's Play - Reversi</title>
+    <title>AI Final Project - Resume Play</title>
     <meta charset="UTF-8" />
     <meta name="keywords"   content="Student AI Project, artificial intellegence, games, reversi" />
-    <meta name"description" content="Play Reversi against the Machine!" />
+    <meta name"description" content="Play Reversi!" />
 
     <link rel="stylesheet" type="text/css" href="css/style.css" />
 </head>
@@ -37,25 +37,24 @@ $board[5][6] = 3;
         <table align="center" border="1">
 
             <?php
-                var_dump($board);
 
-                for ($i = 0; $i < 8; $i++)
+            for ($i = 0; $i < 8; $i++)
+            {
+                echo('<tr>');
+                for ($j = 0; $j < 8; $j++)
                 {
-                    echo('<tr>');
-                    for ($j = 0; $j < 8; $j++)
-                    {
-                        echo('<td>');
-                        if ($board[$i][$j] == 0) {
-                            echo('<img src="img/blank.jpeg" />');
-                        } else if ($board[$i][$j] == 1) {
-                            echo('<img src="img/black.jpeg" />');
-                        } else if ($board[$i][$j] == 2) {
-                            echo('<img src="img/white.jpeg" />');
-                        } else if ($board[$i][$j] == 3) {
-                            echo('<img src="img/valid.jpeg" />');
-                        }
+                    echo('<td>');
+                    if ($board[$i][$j] == 0) {
+                        echo('<img src="img/blank.jpeg" />');
+                    } else if ($board[$i][$j] == 1) {
+                        echo('<img src="img/black.jpeg" />');
+                    } else if ($board[$i][$j] == 2) {
+                        echo('<img src="img/white.jpeg" />');
+                    } else if ($board[$i][$j] == 3) {
+                        echo('<img src="img/valid.jpeg" />');
                     }
                 }
+            }
 
             ?>
 
