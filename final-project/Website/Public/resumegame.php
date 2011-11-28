be1f45d6b9fcffed4adf5cf80689d672eab57a48
James Baiera <jbaiera@kent.edu>
2011-11-28 04:14:12 -0500

    Started the styling for the web interface. This is step one for the user interface. Next step is displaying the valid moves in a list of radio buttons, and getting java script to highlight the spaces they correspond to.

--- final-project/Website/Public/resumegame.php
+++ final-project/Website/Public/resumegame.php
@@ -1,23 +1,45 @@
-<?
+<?php
 
-include("connect.php");
+//include("connect.php");
 
-if (! isset($_SESSION['user_id']))
+/*if (! isset($_SESSION['user_id']))
 {
     header('Location: index.php');
-}
+}*/
 
 $ai_location = '../..';
 
-// echo (system("pwd"));
-
+//initialize the board
 for ($i = 0; $i < 8; $i++)
     for ($j = 0; $j < 8; $j++)
         $board[$i][$j] = 0;
 
+//board testing
 $board[3][4] = 1;
 $board[4][5] = 2;
 $board[5][6] = 3;
+
+function displayGameBoard($boardstate)
+{
+    for ($i = 0; $i < 8; $i++)
+    {
+        echo('<tr>');
+        for ($j = 0; $j < 8; $j++)
+        {
+            echo('<td>');
+            if ($boardstate[$i][$j] == 0) {
+                $imgsrc = 'src="img/blank.jpeg"';
+            } else if ($boardstate[$i][$j] == 1) {
+                $imgsrc = 'src="img/black.jpeg"';
+            } else if ($boardstate[$i][$j] == 2) {
+                $imgsrc = 'src="img/white.jpeg"';
+            } else if ($boardstate[$i][$j] == 3) {
+                $imgsrc = 'src="img/valid.jpeg"';
+            }
+            echo("<img class=\"space\" height=\"50\" width=\"50\" $imgsrc />");
+        }
+    }
+}
 
 ?>
 
@@ -33,33 +55,16 @@ $board[5][6] = 3;
 
 <body>
     <div class="maincontainer">
-
-        <table align="center" border="1">
-
-            <?php
-
-            for ($i = 0; $i < 8; $i++)
-            {
-                echo('<tr>');
-                for ($j = 0; $j < 8; $j++)
-                {
-                    echo('<td>');
-                    if ($board[$i][$j] == 0) {
-                        echo('<img src="img/blank.jpeg" />');
-                    } else if ($board[$i][$j] == 1) {
-                        echo('<img src="img/black.jpeg" />');
-                    } else if ($board[$i][$j] == 2) {
-                        echo('<img src="img/white.jpeg" />');
-                    } else if ($board[$i][$j] == 3) {
-                        echo('<img src="img/valid.jpeg" />');
-                    }
-                }
-            }
-
-            ?>
-
-        </table>
-
+        <div class="boardcontainer">
+            <div class="boardoutline">
+                <h3 class="boardname">Reversi</h3>
+                <table class="reversiboard" border="1">
+                    <?php displayGameBoard($board); ?>
+                </table>
+            </div>
+        </div>
+        <div class="rightside">
+        </div>
     </div>
 </body>
 </html>
