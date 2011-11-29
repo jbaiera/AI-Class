d709578fbd11025141d0aca6b4bbbedebdfbfafa
Nicholas Tietz <ntietz@gmail.com>
2011-11-28 22:45:33 -0500

    Updated game choosing script so all the games for the current logged in player are listed.

--- final-project/Website/Public/choosegame.php
+++ final-project/Website/Public/choosegame.php
@@ -8,25 +8,50 @@ if (! isset($_SESSION['user_id']))
 $user_id = $_SESSION['user_id'];
 
 function displayGames() {
-    global $db;
+    global $db, $user_id;
 
-    echo("<form action='resumegame.php' method='get'>");
-    
+    echo("<ul>");
+
     $stmt = $db->prepare("SELECT game_id, player_1, player_2, game_type FROM games WHERE player_1 = ? or player_2 = ?");
     $stmt->bind_param("ii", $user_id, $user_id);
     $stmt->execute();
     $stmt->bind_result($game_id, $player_1, $player_2, $game_type);
-    $stmt->fetch();
+    $stmt->store_result();
 
-    echo($game_type);
-    echo($game_id);
-    echo($player_1);
-    echo($player_2);
-    echo($game_id);
+    $num_rows = $stmt->num_rows();
+
+    for ($i = 0; $i < $num_rows; $i++) {
+        $stmt->fetch();
+
+        if ($user_id == $player_1) {
+            $color = "black";
+            $human = 1;
+            $opponent = $player_2;
+        } else {
+            $color = "white";
+            $human = 2;
+            $opponent = $player_1;
+        }
+
+        if ($player_1 == $player_2) {
+            $color = 'both';
+            $against = ' for practice';
+        } else {
+            $against = ' against ';
+            if ($opponent = -1) {
+                $against .= "greedy AI";
+            } else {
+                $against .= "unknown player";
+            }
+        }
+
+        echo("<li>");
+        echo("<a href='resumegame.php?game_id=$game_id'>Game as " . $color . $against . "</a>");
+    }
 
     $stmt->close();
 
-    echo("</form>");
+    echo("</ul>");
 }
 
 ?>
