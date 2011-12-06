2e6544f727ce0ab240949f27970a84ce8b99026b
Nicholas Tietz <ntietz@gmail.com>
2011-12-06 01:27:13 -0500

    Created greedy sim 1.

--- final-project/greedySims.hs
+++ final-project/greedySims.hs
@@ -20,22 +20,40 @@ doSimulation xs ys = results
     where pairs = zip xs ys
           results = map (\(x,y) -> simulateScore initialBoard 1 x y) pairs
 
+summarize :: [(GameOverState, Int, Int)] -> String -> String -> IO ()
+summarize results black white = do
+    putStrLn $ black ++ " won " ++ (show bwins) ++ " times."
+    putStrLn $ white ++ " won " ++ (show wwins) ++ " times."
+    putStrLn $ "There were " ++ (show ties) ++ " ties."
+    putStrLn $ black ++ " scored " ++ (show bavg) ++ " on average."
+    putStrLn $ white ++ " scored " ++ (show wavg) ++ " on average."
+    putStrLn $ ""
+    where wwins = foldr (\(x,_,_) n -> if x == 2 then n+1 else n) 0 results
+          bwins = foldr (\(x,_,_) n -> if x == 1 then n+1 else n) 0 results
+          ties  = foldr (\(x,_,_) n -> if x == 0 then n+1 else n) 0 results
+          wavg  = (fromIntegral $ foldr (\(_,_,x) n -> x+n) 0 results) / (fromIntegral $ length results)
+          bavg  = (fromIntegral $ foldr (\(_,x,_) n -> x+n) 0 results) / (fromIntegral $ length results)
+
 main = do
     putStrLn "greedy vs. greedy:"
     let greedyResults = doSimulation greedyList greedyList
     print greedyResults
+    summarize greedyResults "greedy" "greedy"
     --
     putStrLn "greedy vs. rab4hm:"
     let rab4hmResults = doSimulation greedyList rab4hmList
     print rab4hmResults
+    summarize rab4hmResults "greedy" "rab4hm"
     --
     putStrLn "greedy vs. rab6hm:"
     let rab6hmResults = doSimulation greedyList rab6hmList
     print rab6hmResults
+    summarize rab6hmResults "greedy" "rab6hm"
     --
     putStrLn "greedy vs. rab4gr:"
     let rab4grResults = doSimulation greedyList rab4grList
     print rab4grResults
+    summarize rab4grResults "greedy" "rab4gr"
     --
     return ()
 
